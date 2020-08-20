873d239bc20a748f822f1da03ea7034d219bf3c4
Neil Miller <nmiller@allego.com>
2020-08-20 12:58:39 -0400

    Add ‘default’ and ‘sampleValues’ to fields

--- src/main/java/com/giffardtechnologies/restdocs/domain/type/Field.java
+++ src/main/java/com/giffardtechnologies/restdocs/domain/type/Field.java
@@ -1,16 +1,26 @@
 package com.giffardtechnologies.restdocs.domain.type;
 
+import com.google.gson.annotations.SerializedName;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
 public class Field extends TypeSpec {
 	private String name;
 	private String longName = "";
 	private String description;
+	@Nullable
+	@SerializedName("default")
+	private String defaultValue;
 	private boolean required = true;
 	private NamedType mParent;
+	@Nullable
+	private List<String> sampleValues;
 
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -26,15 +36,33 @@ public class Field extends TypeSpec {
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
+	public boolean hasDefaultValue() {
+		return defaultValue != null;
+	}
+
+	// for velocity templating
+	public boolean getHasDefaultValue() {
+		return hasDefaultValue();
+	}
+
+	@Nullable
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+	public void setDefaultValue(@Nullable String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
+
 	public boolean isRequired() {
 		return required;
 	}
-	
+
 	public void setRequired(boolean required) {
 		this.required = required;
 	}
