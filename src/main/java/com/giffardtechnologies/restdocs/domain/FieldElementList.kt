fce7ee32f5128e578beacf2694643f08bfc86460
Neil Miller <nmiller@allego.com>
2023-06-14 18:52:31 -0400

    Add nested exclude to domain object

--- src/main/java/com/giffardtechnologies/restdocs/domain/FieldElementList.kt
+++ src/main/java/com/giffardtechnologies/restdocs/domain/FieldElementList.kt
@@ -1,10 +1,15 @@
 package com.giffardtechnologies.restdocs.domain
 
+import com.giffardtechnologies.restdocs.domain.type.DataType
 import com.giffardtechnologies.restdocs.domain.type.Field
 import com.giffardtechnologies.restdocs.domain.type.NamedType
-import java.util.stream.Collectors
-import kotlin.Boolean
-import kotlin.IllegalStateException
+import com.giffardtechnologies.restdocs.domain.type.TypeSpec
+import com.giffardtechnologies.restdocs.model.FieldPath
+import com.giffardtechnologies.restdocs.model.FieldPathLeaf
+import com.giffardtechnologies.restdocs.model.FieldPathSet
+import com.giffardtechnologies.restdocs.model.FieldPathStem
+import io.vavr.collection.Array
+import io.vavr.collection.HashSet
 
 /**
  * A class that manages a list of fields whether a straight list or a include
@@ -33,25 +38,14 @@ class FieldElementList {
                     if (fieldListElement.excluding.isEmpty()) {
                         newFields.addAll(includedObject.fields)
                     } else {
-                        val fieldNameSet = includedObject.fields
-                            .stream()
-                            .map { obj: Field -> obj.longName }
-                            .collect(Collectors.toSet())
-                        if (!fieldNameSet.containsAll(fieldListElement.excluding)) {
-                            val nonMatching = HashSet(fieldListElement.excluding)
-                            nonMatching.removeAll(fieldNameSet)
-                            throw IllegalStateException(
-                                "Cannot find excluded field(s): " + nonMatching.joinToString(", ")
-                            )
-                        }
-                        val fieldsToAdd = includedObject.fields
-                            .stream()
-                            .filter { field: Field ->
-                                !fieldListElement.excluding
-                                    .contains(field.longName)
-                            }
-                            .collect(Collectors.toList())
-                        fields!!.addAll(fieldsToAdd)
+                        val fieldPaths = fieldListElement.excluding.map { FieldPath(it) }
+                        val excludingPathSet = FieldPathSet.ofAll(fieldPaths)
+
+                        val fields = includedObject.fields
+
+                        val fieldsToAdd = getIncludedFields(fields, excludingPathSet, Array.empty())
+
+                        newFields.addAll(fieldsToAdd)
                     }
                 } else {
                     throw IllegalStateException("Unsupported element type: " + fieldListElement.javaClass.name)
@@ -62,6 +56,80 @@ class FieldElementList {
         }
         return fields
     }
+
+    private fun getIncludedFields(
+        fields: ArrayList<Field>,
+        excludingPathSet: FieldPathSet,
+        parentPath: Array<String>
+    ): ArrayList<Field> {
+        val includedFields = ArrayList<Field>()
+
+        // validate first level fields
+        val fieldNames = fields.stream().map { it.longName }.collect(HashSet.collector())
+        val excludedFieldNames = HashSet.ofAll(excludingPathSet.map { it.field })
+        if (!fieldNames.containsAll(excludedFieldNames)) {
+            val missedExcludes = excludedFieldNames.removeAll(fieldNames)
+            throw IllegalStateException(
+                "'excluding' element refers to unknown field${if (missedExcludes.size() > 1) "s" else ""}: ${
+                    missedExcludes.map { "'" + parentPath.joinToString(separator = ".", postfix = ".") + it + "'" }
+                        .joinToString(
+                            separator = ", "
+                        )
+                }"
+            )
+        }
+
+        fields.forEach { field ->
+            when(val node = excludingPathSet[field.longName]) {
+                is FieldPathLeaf -> {}
+                is FieldPathStem -> {
+                    val newPath = parentPath.append(field.longName)
+                    val subField = Field()
+                    subField.name = field.name
+                    subField.longName = field.longName
+                    subField.parent = field.parent
+                    subField.parentDocument = parentDocument
+                    if (field.typeRef != null) {
+                        val dataObject = parentDocument.getDataObjectByName(field.typeRef)
+                        subField.type = DataType.OBJECT
+                        subField.fields = getIncludedFields(dataObject.fields, node.childPathElements, newPath)
+                    } else if (field.type == DataType.ARRAY && field.items!!.typeRef != null) {
+                        val dataObject = parentDocument.getDataObjectByName(field.items.typeRef)
+                        subField.type = DataType.ARRAY
+                        subField.items = TypeSpec()
+                        subField.items.parentDocument = parentDocument
+                        subField.items.type = DataType.OBJECT
+                        subField.items.fields = getIncludedFields(dataObject.fields, node.childPathElements, newPath)
+                    } else if (field.type == DataType.OBJECT) {
+                        subField.type = DataType.OBJECT
+                        subField.fields = getIncludedFields(field.fields!!, node.childPathElements, newPath)
+                    } else if (field.type == DataType.ARRAY && field.items!!.type == DataType.OBJECT) {
+                        subField.type = DataType.ARRAY
+                        subField.items = TypeSpec()
+                        subField.items.parentDocument = parentDocument
+                        subField.items.type = DataType.OBJECT
+                        subField.items.fields = getIncludedFields(
+                            field.items.fields!!,
+                            node.childPathElements,
+                            newPath
+                        )
+                    } else {
+                        throw IllegalStateException(
+                            "Cannot exclude sub-fields of non-object type (type-ref or object): '${
+                                newPath.joinToString(
+                                    separator = "."
+                                )
+                            }'"
+                        )
+                    }
+                    includedFields.add(subField)
+                }
+                null -> includedFields.add(field)
+            }
+        }
+
+        return includedFields
+   }
 
     fun setFields(fields: ArrayList<Field>) {
         this.fields = ArrayList(fields)
@@ -88,7 +156,7 @@ class FieldElementList {
     private fun linkFields(fields: ArrayList<Field>) {
         for (field in fields) {
             field.parent = parentType
-            field.setParentDocument(parentDocument)
+            field.parentDocument = parentDocument
         }
     }
 
@@ -96,7 +164,7 @@ class FieldElementList {
         this.parentDocument = parentDocument
     }
 
-    fun setParentType(parentType: NamedType) {
+    fun setParentType(parentType: NamedType?) {
         this.parentType = parentType
     }
 }
