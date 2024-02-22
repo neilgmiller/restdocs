ff3c7737e34188bad86ae41f4165507dbedac57e
Neil Miller <nmiller@allego.com>
2024-02-22 15:31:22 -0500

    fixup! Storage model updates

--- src/main/java/com/giffardtechnologies/restdocs/storage/type/EnumConstant.kt
+++ src/main/java/com/giffardtechnologies/restdocs/storage/type/EnumConstant.kt
@@ -9,6 +9,6 @@ package com.giffardtechnologies.restdocs.storage.type
  */
 data class EnumConstant(
     val value: String,
-    val longName: String,
+    val longName: String? = null,
     val description: String? = null
 )
